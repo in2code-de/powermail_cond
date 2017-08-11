fe5f5079174088dcc9809e138c0ba38d6f5644cd
Alex Kellner <alexander.kellner@in2code.de>
2017-08-11 16:36:30 +0200

    [TASK] Code cleanup

--- Classes/UserFunc/GetPowermailFields.php
+++ Classes/UserFunc/GetPowermailFields.php
@@ -1,32 +1,6 @@
 <?php
 namespace In2code\PowermailCond\UserFunc;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2015 in2code.de
- *  Alex Kellner <alexander.kellner@in2code.de>,
- *  Oliver Eglseder <oliver.eglseder@in2code.de>
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 use In2code\Powermail\Domain\Model\Field;
 use In2code\Powermail\Domain\Model\Form;
 use In2code\Powermail\Domain\Model\Page;
@@ -35,11 +9,7 @@ use TYPO3\CMS\Backend\Form\FormEngine;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
- * List powermail fields in Backend for powermail_cond rules
- *
- * @package powermail_cond
- * @license http://www.gnu.org/licenses/lgpl.html
- *            GNU Lesser General Public License, version 3 or later
+ * Class GetPowermailFields
  */
 class GetPowermailFields
 {
@@ -235,6 +205,7 @@ class GetPowermailFields
     /**
      * @param array $params
      * @return void
+     * @SuppressWarnings(PHPMD.Superglobals)
      */
     protected function initialize(array &$params)
     {
