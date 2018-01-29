74b64f8bb0d351ff3c97453ff3cf83ab1f97b169
Alex Kellner <alexander.kellner@in2code.de>
2018-01-29 09:43:41 +0100

    [TASK] Remove TYPO3 7.6 requirements

--- Classes/Utility/ConfigurationUtility.php
+++ Classes/Utility/ConfigurationUtility.php
@@ -1,36 +1,6 @@
 <?php
 namespace In2code\PowermailCond\Utility;
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
-
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2015 in2code.de
- *  Alex Kellner <alexander.kellner@in2code.de>,
- *  Oliver Eglseder <oliver.eglseder@in2code.de>
- *
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-
 /**
  * Class ConfigurationUtility
  */
@@ -45,21 +15,6 @@ class ConfigurationUtility
      */
     public static function getIconPath($fileName)
     {
-        $prefix = 'EXT:powermail_cond/';
-        if (!GeneralUtility::compat_version('7.6')) {
-            $prefix = ExtensionManagementUtility::extRelPath('powermail_cond');
-        }
-        $iconPath = $prefix . 'Resources/Public/Icons/' . $fileName;
-        return $iconPath;
-    }
-
-    /**
-     * Check if TYPO3 smaller then 8.7 is running
-     *
-     * @return bool
-     */
-    public static function isOlderThan8Lts()
-    {
-        return VersionNumberUtility::convertVersionNumberToInteger(TYPO3_version) < 8007000;
+        return 'EXT:powermail_cond/Resources/Public/Icons/' . $fileName;
     }
 }
