91205fbdfb115c0804c7436ea6304c5c7d081bbf
Alexander Kellner <alexander.kellner@in2code.de>
2021-11-22 12:11:16 +0100

    [BUGFIX] Use int type for condition.rule table field

--- ext_tables.sql
+++ ext_tables.sql
@@ -42,7 +42,7 @@ CREATE TABLE tx_powermailcond_domain_model_condition (
 	conditioncontainer int(11) DEFAULT '0' NOT NULL,
 
 	title tinytext NOT NULL,
-	target_field tinytext NOT NULL,
+	target_field int(11) DEFAULT '0' NOT NULL,
 	actions tinytext NOT NULL,
 	conjunction tinytext NOT NULL,
 
@@ -69,7 +69,7 @@ CREATE TABLE tx_powermailcond_domain_model_rule (
 	ops int(11) DEFAULT '0' NOT NULL,
 	cond_string text NOT NULL,
 	equal_field int(11) DEFAULT '0' NOT NULL,
-	
+
 	PRIMARY KEY (uid),
 	KEY parent (pid),
 	KEY conditions (conditions),
