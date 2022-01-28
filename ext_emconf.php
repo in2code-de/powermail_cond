315e6aef38108755eb903a766454f5de43577819
Stefan Busemann <stefan.busemann@in2code.de>
2022-01-28 17:02:32 +0100

    [TASK] change system requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,15 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Powermail Conditions',
     'description' => 'Add conditions (via AJAX) to powermail forms for fields and pages',
     'category' => 'plugin',
-    'version' => '8.2.2',
+    'version' => '8.3.0',
     'state' => 'stable',
     'author' => 'Alex Kellner',
     'author_email' => 'alexander.kellner@in2code.de',
     'author_company' => 'in2code.de',
     'constraints' => [
         'depends' => [
-            'powermail' => '8.0.0-8.99.99',
-            'typo3' => '10.4.0-10.4.99'
+            'powermail' => '8.0.0-9.99.99',
+            'typo3' => '10.4.0-11.45.99'
         ],
         'conflicts' => [
         ],
