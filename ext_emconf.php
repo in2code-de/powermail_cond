866598ac97986fa40b7d4141f0b038253ac28751
Stephan Salzmann <stephan.salzmann@in2code.de>
2023-04-13 16:16:29 +0200

    [TASK] Set temporary dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '12.2.0-12.5.99',
             'php' => '8.1.0 - 8.2.99',
-            'powermail' => '*',
+            'powermail' => 'dev-v12_compat',
         ],
         'conflicts' => [
         ],
