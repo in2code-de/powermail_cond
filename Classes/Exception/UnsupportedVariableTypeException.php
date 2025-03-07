415bc0aafc00b06369a36955d82f1f5889b67e76
Marcus Schwemer <marcus.schwemer@in2code.de>
2025-03-07 16:42:08 +0100

    [TASK] Make exceptions compatible with php 8.4
    Related: in2code-pro/powermail_cond#9

--- Classes/Exception/UnsupportedVariableTypeException.php
+++ Classes/Exception/UnsupportedVariableTypeException.php
@@ -12,7 +12,7 @@ class UnsupportedVariableTypeException extends Exception
     private const MESSAGE = 'A value for a field can only be array or string';
     public const CODE = 1588236757;
 
-    public function __construct(Throwable $previous = null)
+    public function __construct(?Throwable $previous = null)
     {
         parent::__construct(self::MESSAGE, self::CODE, $previous);
     }
