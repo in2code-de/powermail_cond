0f42c30e693e77a63d8a8b7f0b809977415b4a3d
Marcus Schwemer <marcus.schwemer@in2code.de>
2025-03-07 16:42:08 +0100

    [TASK] Make exceptions compatible with php 8.4
    Related: in2code-pro/powermail_cond#9

--- Classes/Exception/MissingPowermailParameterException.php
+++ Classes/Exception/MissingPowermailParameterException.php
@@ -12,7 +12,7 @@ class MissingPowermailParameterException extends Exception
     private const MESSAGE = 'No powermail parameters are given - e.g. &tx_powermail_pi1[mail][form]=123';
     public const CODE = 1643809128416;
 
-    public function __construct(Throwable $previous = null)
+    public function __construct(?Throwable $previous = null)
     {
         parent::__construct(self::MESSAGE, self::CODE, $previous);
     }
